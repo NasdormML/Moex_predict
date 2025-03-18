e58434cfda93c4e98e63a2af4ff1e503273fd354
Herman K. <123388703+NasdormML@users.noreply.github.com>
2025-03-18 20:13:13 +0400

    Add 2 variebles model for sber ticker

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,7 +4,7 @@ from app.models import build_model
 class TestModels(unittest.TestCase):
     def test_build_model(self):
         seq_length = 20
-        num_features = 11  # Количество признаков
+        num_features = 11
         model = build_model(seq_length, num_features, lstm_units=128, gru_units=64, dense_units=64, dropout_rate=0.15, learning_rate=0.001)
         self.assertIsNotNone(model)
         self.assertEqual(model.input_shape, (None, seq_length, num_features))
